fc4a871ebe5a5ac2641d38914d795bcbb879612e
Alexandru Ciornei <alexstefciornei22@gmail.com>
2025-05-18 22:29:40 +0300

    comment out security config

--- src/main/java/ro/upb/acs/worldcuptickets/ticketservice/config/JWTSecurityConfig.java
+++ src/main/java/ro/upb/acs/worldcuptickets/ticketservice/config/JWTSecurityConfig.java
@@ -1,11 +1,6 @@
 package ro.upb.acs.worldcuptickets.ticketservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class JWTSecurityConfig {
