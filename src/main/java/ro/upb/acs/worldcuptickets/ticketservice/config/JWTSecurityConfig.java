6896fe6b622566dc7ee5d684e3a8b2e940c07b05
Alexandru Ciornei <alexstefciornei22@gmail.com>
2025-05-18 22:13:30 +0300

    fix request matchers

--- src/main/java/ro/upb/acs/worldcuptickets/ticketservice/config/JWTSecurityConfig.java
+++ src/main/java/ro/upb/acs/worldcuptickets/ticketservice/config/JWTSecurityConfig.java
@@ -18,8 +18,8 @@ public class JWTSecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers("/api/v1/tickets/users/**").hasAuthority("SCOPE_ticket.read")
-                .requestMatchers(HttpMethod.POST, "/api/v1/tickets/matches/**").hasAuthority("SCOPE_ticket.write")
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/tickets/**").hasAuthority("SCOPE_ticket.delete")
+                .requestMatchers( "/api/v1/tickets/matches/**").hasAuthority("SCOPE_ticket.write")
+                .requestMatchers("/api/v1/tickets/**").hasAuthority("SCOPE_ticket.delete")
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
